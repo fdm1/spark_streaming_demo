5ff0e8c219ba57f01a73be2c44a96499b5bdd121
fdm1 <frankmassi@gmail.com>
2017-02-03 18:57:47 +0000

    initial commit

---
+++ course_scripts/UdemySparkScala/src/main/scala/com/sundogsoftware/sparkstreaming/App.scala
@@ -0,0 +1,26 @@
+package com.sundogsoftware.sparkstreaming
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * @author ${user.name}
+  */
+object App {
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf()
+      .setAppName("The swankiest Spark app ever")
+      .setMaster("local[2]")
+
+    val sc = new SparkContext(conf)
+
+    val col = sc.parallelize(0 to 100 by 5)
+    val smp = col.sample(true, 4)
+    val colCount = col.count
+    val smpCount = smp.count
+
+    println("orig count = " + colCount)
+    println("sampled count = " + smpCount)
+  }
+
+}
---
+++ spark_udemy_setup.sh
@@ -0,0 +1,15 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $DIR
+
+if [ ! -d docker-spark ]; then
+  git clone https://github.com/gettyimages/docker-spark
+fi
+
+cp docker-spark/docker-compose.yml .
+
+sed -i 's%- ./%- ./docker-spark/%' docker-compose.yml
+sed -i '/\/docker-spark\/data/ a \    - ./course_scripts:/course_scripts' docker-compose.yml
+
+cd course_scripts
+mvn archetype:generate -B -DarchetypeCatalog=https://github.com/mbonaci/spark-archetype-scala/raw/master/archetype-catalog.xml -DarchetypeRepository=https://github.com/mbonaci/spark-archetype-scala/raw/master -DarchetypeGroupId=org.sia -DarchetypeArtifactId=spark-archetype-scala -DarchetypeVersion=0.9 -DgroupId=com.frankmassi -DartifactId=UdemySparkScala -Dversion=0.1 -Dpackage=org.sijaset
