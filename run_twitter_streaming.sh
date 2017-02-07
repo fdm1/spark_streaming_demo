a2e9bb58cbf11d5620ce1496ab924896b9149e4a
Debian <code@getbraintree.com>
2017-02-07 13:12:32 +0000

    reorg and better setup script

--- run_twitter_streaming.sh
+++ run_twitter_streaming.sh
@@ -1,7 +1,7 @@
-SCALA_DIR=/course_scripts/SparkScala
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCALA_DIR=/SparkScala
 
-
-mvn install -f $(pwd)/$SCALA_DIR/pom.xml
+mvn install -f $DIR/$SCALA_DIR/pom.xml
 docker-compose run --rm master spark-submit \
   --jars $SCALA_DIR/jars/spark-core_2.11-1.5.2.logging.jar \
   --class com.frankmassi.sparkstreaming.$1 $SCALA_DIR/target/SparkScala-0.1.jar
--- spark_setup.sh
+++ spark_setup.sh
@@ -6,11 +6,18 @@ if [ ! -d docker-spark ]; then
   git clone https://github.com/gettyimages/docker-spark
 fi
 
+if [ ! -d datasets ]; then
+  git clone https://github.com/ChitturiPadma/datasets
+fi
+
 cp docker-spark/docker-compose.yml .
 
 sed -i 's%- ./%- ./docker-spark/%' docker-compose.yml
-sed -i '/\/docker-spark\/data/ a \    - ./course_scripts:/course_scripts' docker-compose.yml
 
-cd course_scripts/SparkScala
-mvn install
+dirs="course_scripts SparkScala datasets twitter_utils"
+for d in $dirs
+do
+  sed -i "/\/docker-spark\/data/ a \    - ./$d:/$d" docker-compose.yml
+done
+
 # mvn archetype:generate -B -DarchetypeCatalog=https://github.com/mbonaci/spark-archetype-scala/raw/master/archetype-catalog.xml -DarchetypeRepository=https://github.com/mbonaci/spark-archetype-scala/raw/master -DarchetypeGroupId=org.sia -DarchetypeArtifactId=spark-archetype-scala -DarchetypeVersion=0.9 -DgroupId=com.frankmassi -DartifactId=UdemySparkScala -Dversion=0.1 -Dpackage=org.sijaset
