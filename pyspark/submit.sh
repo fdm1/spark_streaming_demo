51aec1a6eedbfe29a2a45dc723dc966d5ffe4bbf
Frank Massi <frank.massi@getbraintree.com>
2017-02-12 20:43:57 -0600

    resolved spark errors...not sure if it's actually doing anything

--- pyspark/submit.sh
+++ pyspark/submit.sh
@@ -5,5 +5,5 @@ JARS=$SPARK/jars
 spark-submit \
   --jars \
 $JARS/spark-core_2.11-1.5.2.logging.jar,\
-$JARS/spark-streaming-kafka-assembly_2.10-1.6.3.jar\
+$JARS/spark-streaming-kafka-assembly_2.11-1.6.3.jar\
 	$SPARK/tweet_consumer.py
--- pyspark/tweet_consumer.py
+++ pyspark/tweet_consumer.py
@@ -9,24 +9,23 @@ import logging
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
+import os
 
+kafka = 'kafka:9092'
+zookeeper = 'zookeeper:2181'
+topic = 'tweets'
 
-def get_stream(sc):
+def create_rdd(sc, host=zookeeper):
+    from pyspark.streaming.kafka import OffsetRange
+    return KafkaUtils.createRDD(sc, {'metadata.broker.list': host}, [OffsetRange('tweets', 1,0,100)])
+
+def get_stream(sc, host=kafka):
     ssc = StreamingContext(sc, 1)
-    zookeeper = 'zookeeper:2181'
-    topic = 'tweets'
-    # createDirectStream breaks...why?
-    # kvs = KafkaUtils.createDirectStream(ssc, [topic], {'metadata.broker.list': zookeeper})
-
-    kvs = KafkaUtils.createStream(ssc,
-                                  zookeeper,
-                                  'twitter-consumer',
-                                  {'tweets': 1})
+    kvs = KafkaUtils.createDirectStream(ssc, [topic], {'metadata.broker.list': host})
     return ssc, kvs
 
-
 def run_stream(ssc):
-    # ssc.checkpoint("/tmp/checkpoint/")
+    ssc.checkpoint("/tmp/checkpoint/")
     ssc.start()
     ssc.awaitTermination()
 
@@ -39,7 +38,18 @@ if __name__ == "__main__":
     ssc, kvs = get_stream(sc)
 
     # kvs.checpoint(1)
-    kvs.pprint(10)
+    # kvs.saveAsTextFiles(os.path.expanduser('~') + '/tweets', suffix='txt')
+
+    lines = kvs.map(lambda x: x[1])
+    lines.pprint() 
+    counts = lines.flatMap(lambda line: line.split(" ")) \
+                    .map(lambda word: (word, 1)) \
+                            .reduceByKey(lambda a, b: a+b)
+    logger.error(counts)
+    counts.pprint()
+    counts.saveAsTextFiles(os.path.expanduser('~') + '/tweets_counts')
+    # kvs.pprint()
+    # print(kvs)
     run_stream(ssc)
     # def store(rdd):
     #     import time
@@ -49,15 +59,6 @@ if __name__ == "__main__":
     #     repartitionedRDD.saveAsTextFile("Tweets")
     # # // Stop once we've collected 1000 tweets.
     #
-    # kvs.foreachRDD(store)
-    # lines = kvs.map(lambda x: str(x))
-    # lines.pprint() 
-    # LOG.warn(lines)
-    # counts = lines.flatMap(lambda line: line.split(" ")) \
-    #                 .map(lambda word: (word, 1)) \
-    #                         .reduceByKey(lambda a, b: a+b)
-    # logger.error(counts)
-    # counts.pprint()
 
 
 
